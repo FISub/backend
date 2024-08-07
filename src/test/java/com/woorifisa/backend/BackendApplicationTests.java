d5443c4e4d1efd55c57bcbf41e24e58a56f93864
Been980804 <dlgusqls980804@naver.com>
2024-08-07 17:57:56 +0900

    init:초기설정

---
+++ src/test/java/com/woorifisa/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woorifisa.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
