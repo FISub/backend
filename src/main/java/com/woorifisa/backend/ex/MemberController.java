9935271356a5271e557c7774f6e2ccfd689bd78b
leesj <leesj000603@daum.net>
2024-08-11 17:29:07 +0900

    fix : swagger 사용을 위해 membercontroller의 @Controller 를 @RestController로 수정

--- src/main/java/com/woorifisa/backend/ex/MemberController.java
+++ src/main/java/com/woorifisa/backend/ex/MemberController.java
@@ -1,14 +1,14 @@
 package com.woorifisa.backend.ex;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class MemberController {
     
