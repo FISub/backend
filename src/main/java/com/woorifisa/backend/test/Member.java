9f80bf50a1ce80fdcb7aaf50992566d90b8a9113
Been980804 <dlgusqls980804@naver.com>
2024-08-12 18:42:23 +0900

    fix: 안쓰는 import삭제

--- src/main/java/com/woorifisa/backend/test/Member.java
+++ src/main/java/com/woorifisa/backend/test/Member.java
@@ -2,7 +2,6 @@ package com.woorifisa.backend.test;
 
 import java.util.Date;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
--- src/main/java/com/woorifisa/backend/test/MemberDTO.java
+++ src/main/java/com/woorifisa/backend/test/MemberDTO.java
@@ -2,7 +2,6 @@ package com.woorifisa.backend.test;
 
 import java.util.Date;
 
-import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
