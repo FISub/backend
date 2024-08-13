b213a3d19a1e3cef34f8658479691104ebc638de
leesj <leesj000603@daum.net>
2024-08-13 22:56:37 +0900

    feat : join exception 추가

---
+++ src/main/java/com/woorifisa/backend/member/exception/JoinException.java
@@ -0,0 +1,9 @@
+package com.woorifisa.backend.member.exception;
+
+public class JoinException extends Exception{
+    
+    public JoinException() {}
+	public JoinException(String message) {
+		super(message);
+	}
+}
---
+++ src/main/java/com/woorifisa/backend/member/exception/LoginException.java
@@ -0,0 +1,9 @@
+package com.woorifisa.backend.member.exception;
+
+public class LoginException extends Exception{
+    
+    public LoginException() {}
+	public LoginException(String message) {
+		super(message);
+	}
+}
