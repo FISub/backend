4b92268c4e7d45c80fc692edd4416087e38f50c1
Been980804 <dlgusqls980804@naver.com>
2024-08-13 18:56:18 +0900

    feat:Product테이블 카테고리 컬럼 추가로 인한 수정

--- src/main/java/com/woorifisa/backend/common/repository/ProductRepository.java
+++ src/main/java/com/woorifisa/backend/common/repository/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.woorifisa.backend.common.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -10,15 +12,19 @@ import com.woorifisa.backend.common.entity.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
-
+    // 상품 추가
     @Modifying
-    @Query(value = "INSERT INTO product (prod_name, prod_price, prod_intro, prod_img, mem_num) VALUES (:prodName, :prodPrice, :prodIntro, :prodImg, :memNum)", nativeQuery = true)
-    void insertProduct(
+    @Query(value = "INSERT INTO product (prod_name, prod_price, prod_intro, prod_img, prod_cat, mem_num) VALUES (:prodName, :prodPrice, :prodIntro, :prodImg, :prodCat,:memNum)", nativeQuery = true)
+    public void insertProduct(
         @Param("prodName") String prodName,
         @Param("prodPrice") int prodPrice,
         @Param("prodIntro") String prodIntro,
         @Param("prodImg") String prodImg,
+        @Param("prodCat") int prodCat,
         @Param("memNum") String memNum
     );
-    
+    // main 상품 미리보기 list 출력
+    @Modifying
+    @Query(value= "select * from product order by prod_num desc limit 6", nativeQuery = true)
+    public List<Product> preview();
 }
