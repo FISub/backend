2d47ca3880589e8336dcee967fac928e634b0d3c
leesj000603 <leesj000603@daum.net>
2024-08-14 14:33:37 +0900

    feat : session-cookie 를 통한 로그인 정보 조회 및 회원가입, 로그인, 로그아웃 api

--- src/main/java/com/woorifisa/backend/member/service/AuthServiceImpl.java
+++ src/main/java/com/woorifisa/backend/member/service/AuthServiceImpl.java
@@ -8,12 +8,17 @@ import org.springframework.transaction.annotation.Transactional;
 import com.woorifisa.backend.common.dto.MemberDTO;
 import com.woorifisa.backend.common.entity.Member;
 import com.woorifisa.backend.common.repository.MemberRepository;
+import com.woorifisa.backend.member.dto.LoginResponseDTO;
+import com.woorifisa.backend.member.dto.LoginSessionDTO;
+import com.woorifisa.backend.member.dto.MemberInfoDTO;
 import com.woorifisa.backend.member.exception.JoinException;
 import com.woorifisa.backend.member.exception.LoginException;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 @Service
+
 public class AuthServiceImpl implements AuthService {
 
     @Autowired
@@ -22,16 +27,25 @@ public class AuthServiceImpl implements AuthService {
     private ModelMapper mapper = new ModelMapper(); 
 
     @Override
-    public String login(String id, String pw, HttpSession session) throws LoginException {
+    public LoginResponseDTO login(String id, String pw, HttpServletRequest request) throws LoginException {
         Member member = memberRepository.findByMemId(id).orElse(null);
         System.out.println(member);
         if (member != null) {
             if (pw.equals(member.getMemPw())) {
-                MemberDTO dto = mapper.map(member, MemberDTO.class);
-                System.out.println(dto);
-                session.setAttribute("userId", id);
-                session.setAttribute("userRole", member.getMemType());
-                return "성공적으로 로그인 되었습니다.";
+                LoginSessionDTO login = mapper.map(member, LoginSessionDTO.class);
+                System.out.println(login);
+                HttpSession session = request.getSession();
+                System.out.println("저장된 세션 id: "+ session.getId() );
+                session.setAttribute("login", login);
+
+                System.out.println("저장된 세션 : "+session.getAttribute("login")); 
+
+                return LoginResponseDTO.builder()
+                                       .memNum(member.getMemNum())
+                                       .memId(id)
+                                       .memType(member.getMemType())
+                                       .build();
+                                                                            
             } else {
                 throw new LoginException("비밀번호가 다릅니다.");
             }
@@ -42,11 +56,11 @@ public class AuthServiceImpl implements AuthService {
 
     @Transactional
     @Override
-    public String join(MemberDTO memberDTO) throws JoinException {
+    public String join(MemberInfoDTO memberInfoDTO) throws JoinException {
         try{
-            memberRepository.insertMem(memberDTO.getMemName(), memberDTO.getMemId(), memberDTO.getMemPw(), 
-                                       memberDTO.getMemEmail(), memberDTO.getMemPhone(), memberDTO.getMemSex(), 
-                                       memberDTO.getMemAddr(), memberDTO.getMemBirth(), memberDTO.getMemType());
+            memberRepository.insertMem(memberInfoDTO.getMemName(), memberInfoDTO.getMemId(), memberInfoDTO.getMemPw(), 
+                                       memberInfoDTO.getMemEmail(), memberInfoDTO.getMemPhone(), memberInfoDTO.getMemSex(), 
+                                       memberInfoDTO.getMemAddr(), memberInfoDTO.getMemBirth(), memberInfoDTO.getMemType());
         } catch (Exception e) {
             e.printStackTrace();
             throw new JoinException("회원가입 실패");
@@ -55,6 +69,20 @@ public class AuthServiceImpl implements AuthService {
         
     }
 
+    @Override
+    public boolean isValidSession(String sessionId, HttpSession session){
+        if (sessionId == null || !sessionId.equals(session.getId())) {
+            return false;
+        } else {
+            // 세션에 userId 속성이 있는지 확인하여 유효성을 판단
+            String userId = (String) session.getAttribute("userId");
+            return userId != null;
+        }        
+    }
 
-
+    @Override
+    public String logout() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'logout'");
+    }
 }
