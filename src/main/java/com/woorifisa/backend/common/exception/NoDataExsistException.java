3089084660f2cb8854744c6b844648c80effc621
leesj <leesj000603@daum.net>
2024-08-15 15:02:18 +0900

    feat : 조회한 데이터 결과가 null일 경우 사용할 Exception 추가

---
+++ src/main/java/com/woorifisa/backend/common/exception/NoDataExsistException.java
@@ -0,0 +1,8 @@
+package com.woorifisa.backend.common.exception;
+
+public class NoDataExsistException extends Exception {
+    public NoDataExsistException() {}
+	public NoDataExsistException(String message) {
+		super(message);
+	}
+}
