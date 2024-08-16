a267c094d4389b4ff92e15c3b7970b693107a713
Been980804 <dlgusqls980804@naver.com>
2024-08-16 09:42:16 +0900

    feat:test 패키지 삭제

--- src/main/java/com/woorifisa/backend/test/LoginException.java
+++
@@ -1,9 +0,0 @@
-package com.woorifisa.backend.test;
-
-public class LoginException extends Exception{
-    
-    public LoginException() {}
-	public LoginException(String message) {
-		super(message);
-	}
-}
