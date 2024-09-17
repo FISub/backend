fcd08d4651b58b2b7e2f2d116acc07b14a71c483
Been980804 <dlgusqls980804@naver.com>
2024-09-17 13:24:48 +0900

    fix:LoginRequestDTO에 기본생성자 어노테이션 추가

--- src/main/java/com/woorifisa/backend/member/dto/LoginRequestDTO.java
+++ src/main/java/com/woorifisa/backend/member/dto/LoginRequestDTO.java
@@ -2,10 +2,12 @@ package com.woorifisa.backend.member.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Setter
 @Getter
 @ToString
