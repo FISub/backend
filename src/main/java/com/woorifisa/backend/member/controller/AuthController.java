8b4d267538b9a2d31f60799219e646285e624ab0
leesj <leesj000603@daum.net>
2024-09-18 22:11:43 +0900

    feat : 로그인 및 로그아웃 기능 삭제 및 세션 정보 조회 api 주석처리 (추후 구현)

--- src/main/java/com/woorifisa/backend/member/controller/AuthController.java
+++ src/main/java/com/woorifisa/backend/member/controller/AuthController.java
@@ -28,37 +28,15 @@ public class AuthController {
 
     @Autowired
     AuthService authService;
-    // 로그인(test)
-    @PostMapping("/login")
-    @Operation(summary = "로그인 (개발 완료)", description = "로그인 성공 시 id와 권한 정보를 session에 저장")
-    public LoginResponseDTO login(@RequestBody LoginRequestDTO loginDTO, HttpServletRequest request) throws LoginException{
-        LoginResponseDTO loginResponseDTO = authService.login(loginDTO.getId(), loginDTO.getPw(), request);  
-        return loginResponseDTO;
-    }
+    
 
     @PostMapping("/join")
     @Operation(summary = "회원가입 (개발 완료)", description = "회원가입 시 회원 정보를 저장")
     public String join(@RequestBody MemberInfoDTO memberInfoDTO) throws JoinException{
         return authService.join(memberInfoDTO);
     }
-
-    @PostMapping("/logout")
-    @Operation(summary = "로그아웃 (개발 완료 - 그러나 예외처리 추가 해야함)", description = "로그아웃")
-    public String logout(HttpServletRequest request, HttpServletResponse response) throws LoginException{ 
-        HttpSession session = request.getSession();
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if ("JSESSIONID".equals(cookie.getName())) {
-                    cookie.setMaxAge(0); // 쿠키 만료 시간을 0으로 설정
-                    cookie.setPath("/"); // 쿠키의 경로를 설정 (기본값)
-                    response.addCookie(cookie); // 변경된 쿠키를 클라이언트에 전송
-                }
-            }
-        }
-        return authService.logout(session);
-    }
-
+    // 세션 정보 조회 로직 수정해야함
+    /*
     @GetMapping("/sessionInfo")
     @Operation(summary = "세션 정보 조회 (개발 완료 - 그러나 예외처리 추가 해야함)", description = "현재 로그인된 사용자의 세션 정보를 반환합니다.")
     public LoginSessionDTO getSessionInfo(HttpServletRequest request) {
@@ -76,4 +54,5 @@ public class AuthController {
             return null; 
         }
     }
+         */
 }
