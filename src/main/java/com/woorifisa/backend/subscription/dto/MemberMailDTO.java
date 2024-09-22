8cf24d2467bfd747fc1bea5aabd7ee9092f0d262
Been980804 <dlgusqls980804@naver.com>
2024-09-22 20:23:06 +0900

    feat:정기결제 실패시 구독 자동취소 및 메일 알림 로직 구현

---
+++ src/main/java/com/woorifisa/backend/subscription/dto/MemberMailDTO.java
@@ -0,0 +1,19 @@
+package com.woorifisa.backend.subscription.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+@Builder
+public class MemberMailDTO {
+    private String memEmail;
+    private String memName;
+}
