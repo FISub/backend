f5384571500a3c30058175fc910d2a929f070098
Been980804 <dlgusqls980804@naver.com>
2024-09-22 23:13:26 +0900

    feat:결제 성공시 메일 전송

--- src/main/java/com/woorifisa/backend/subscription/util/SendMail.java
+++ src/main/java/com/woorifisa/backend/subscription/util/SendMail.java
@@ -1,26 +1,41 @@
 package com.woorifisa.backend.subscription.util;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import jakarta.mail.internet.MimeMessage;
+import java.io.File;
 
 @Service
 public class SendMail {
-    
+
     @Autowired
     private JavaMailSender mailSender;
 
-    public void sendEmail(String to, String subject, String text){
+    public void sendEmail(String to, String subject, String text) {
         try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setTo(to);
-            message.setSubject(subject);
-            message.setText(text);
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8"); // true를 설정하여 멀티파트 활성화
+
+            helper.setTo(to);
+            helper.setSubject(subject);
+
+            // HTML 본문 설정, 이미지를 CID로 참조
+            String htmlMsg = "<html><body>" + text + "<br><br><img src='cid:logoImage' style='width:50%;'></body></html>";
+
+            helper.setText(htmlMsg, true); // true로 설정하여 HTML 내용을 허용
+
+            // CID로 참조되는 이미지 첨부
+            FileSystemResource res = new FileSystemResource(new File("src/main/resources/static/Logo-removebg-preview.png"));
+            helper.addInline("logoImage", res);
+
             mailSender.send(message);
-            System.out.println("Mail sent successfully");
+            System.out.println("Mail with inline image sent successfully");
         } catch (Exception e) {
-            System.out.println("Failed to send mail: " + e.getMessage());
+            System.out.println("Failed to send mail with inline image: " + e.getMessage());
             e.printStackTrace();
         }
     }
